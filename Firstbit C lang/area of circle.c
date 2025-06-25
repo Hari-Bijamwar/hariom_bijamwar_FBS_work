8976a0b9e5b86254ce8cdb64ee04f3c7e271856d
Hari-Bijamwar <131506507+Hari-Bijamwar@users.noreply.github.com>
2025-06-25 22:39:02 +0530

    Add files via upload

---
+++ Firstbit C lang/area of circle.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	float r=3;
+	float area = 3.14*r*r;
+	printf("area of circle is %f", area);
+}
---
+++ Firstbit C lang/minute to hour.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int min=130;
+	int hours=min/60;
+	int minutes=min%60;
+	printf("%d minutes is equal to %d hours and %d minutes ",min,hours,minutes);
+}
