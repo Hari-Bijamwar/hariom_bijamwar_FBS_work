41aa3052f32a98a31bb29f77c1ed5a25696c37c4
Hari-Bijamwar <131506507+Hari-Bijamwar@users.noreply.github.com>
2025-07-03 21:46:52 +0530

    Add files via upload

---
+++ C module /Assignment 4/Armstrong numbers in given range.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+void main()
+{
+	int start,end;
+	printf("Enter start of the range \n");
+	scanf("%d",&start);
+	printf("Enter end of the range \n");
+	scanf("%d",&end);
+	int num;
+	for(num=start;num<=end;num++)
+	{
+	int count=0,sum=0;
+	int temp=num;
+	
+	while(temp>0)
+	{
+		temp=temp/10;
+		count++;
+	}
+	temp=num;
+	while(temp>0)
+	{
+		int mod=temp%10;
+		temp=temp/10;
+		int i=0,result=1;
+		while(i<count)
+		{
+		result= result*mod;
+			i++;
+			
+		}
+		sum=sum+result;
+		
+	}
+	if(sum==num)
+	{
+		printf("%d ",num);
+	}
+
+	}
+	
+}
